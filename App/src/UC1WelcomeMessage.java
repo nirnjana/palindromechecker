786268103dffa9a8e73396047700e5166571aed8
nirnjana <niranjanarajesh2005@gmail.com>
2026-02-25 09:29:29 +0530

    Added separate files for all use cases

---
+++ App/src/UC1WelcomeMessage.java
@@ -0,0 +1,23 @@
+public class UC1WelcomeMessage {
+
+    public static void main(String[] args) {
+
+        // Original String
+        String input = "level";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        // Compare original and reversed string
+        if (input.equals(reversed)) {
+            System.out.println("The given string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
---
+++ App/src/UC3UserInputPalindrome.java
@@ -0,0 +1,2 @@
+public class UC3UserInputPalindrome {
+}
