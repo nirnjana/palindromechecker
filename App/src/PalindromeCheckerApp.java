9cd540d0d61e1828a3df6dcc2755c9c6e0fb221e
nirnjana <niranjanarajesh2005@gmail.com>
2026-02-25 10:58:35 +0530

    Added UC13-PerformanceComparison logic

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,29 +1,56 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
+    // Method to check palindrome using String reverse
+    public static boolean checkReverse(String str){
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    // Method to check palindrome using Stack
+    public static boolean checkStack(String str){
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()) stack.push(c);
+        String reversed = "";
+        while(!stack.isEmpty()) reversed += stack.pop();
+        return str.equals(reversed);
+    }
+
+    // Method to check palindrome using Deque
+    public static boolean checkDeque(String str){
+        Deque<Character> deque = new ArrayDeque<>();
+        for(char c : str.toCharArray()) deque.addLast(c);
+        while(deque.size() > 1){
+            if(deque.removeFirst() != deque.removeLast()) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
 
-        LinkedList<Character> list = new LinkedList<>();
-        for(char c : input.toCharArray()){
-            list.add(c);
-        }
+        long start, end;
 
-        boolean isPalindrome = true;
-        while(list.size() > 1){
-            if(!list.removeFirst().equals(list.removeLast())){
-                isPalindrome = false;
-                break;
-            }
-        }
+        start = System.nanoTime();
+        checkReverse(input);
+        end = System.nanoTime();
+        System.out.println("Reverse method took: " + (end-start) + " ns");
 
-        if(isPalindrome){
-            System.out.println(input + " is a palindrome (using LinkedList).");
-        } else {
-            System.out.println(input + " is NOT a palindrome (using LinkedList).");
-        }
+        start = System.nanoTime();
+        checkStack(input);
+        end = System.nanoTime();
+        System.out.println("Stack method took: " + (end-start) + " ns");
+
+        start = System.nanoTime();
+        checkDeque(input);
+        end = System.nanoTime();
+        System.out.println("Deque method took: " + (end-start) + " ns");
+
         sc.close();
     }
 }
